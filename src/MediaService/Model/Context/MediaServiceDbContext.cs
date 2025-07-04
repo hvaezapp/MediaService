2a8f88051e5c75827c5e5d8981d0cf0929a45423
hassa <hassa@LAPTOP-U03CN6LR>
2025-07-04 14:57:52 +0330

    add model and db context

---
+++ src/MediaService/Model/Context/MediaServiceDbContext.cs
@@ -0,0 +1,10 @@
+﻿using MediaService.Model.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaService.Model.Context
+{
+    public class MediaServiceDbContext(DbContextOptions<MediaServiceDbContext> options) : DbContext(options)
+    {
+        public DbSet<UrlToken> Tokens { get; set; }
+    }
+}
---
+++ src/MediaService/Model/Domain/UrlToken.cs
@@ -0,0 +1,12 @@
+﻿namespace MediaService.Model.Domain
+{
+    public class UrlToken
+    {
+        public Guid Id { get; set; }
+        public required string BucketName { get; set; }
+        public required string ObjectName { get; set; }
+        public required string ContentType { get; set; }
+        public DateTime ExpireOn { get; set; }
+        public int AccessCount { get; set; }
+    }
+}
